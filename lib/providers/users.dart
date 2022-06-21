988a2868e35a8a227fef3659fe8b4395a3eeda9b
blitzbeule <38007250+blitzbeule@users.noreply.github.com>
2022-06-21 20:12:15 +0200

    add userProvider

---
+++ lib/providers/users.dart
@@ -0,0 +1,63 @@
+import 'package:amse/api.dart';
+import 'package:amse_api_client/amse_api_client.dart';
+import 'package:amse_api_client/models.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserNotifier extends StateNotifier<List<User>> {
+  final Ref ref;
+
+  Future<void>? _refreshing;
+
+  UserNotifier(this.ref) : super([]) {
+    refresh();
+  }
+
+  Future<void> refresh() {
+    Future<void> _internalRefresh() async {
+      AmseApi api = ref.read(apiProvider);
+
+      final allUsers = await api.users.getAll();
+
+      List<User> result = [];
+
+      for (var mUser in allUsers) {
+        User u = await api.users.getOne(mUser.id);
+        result.add(u);
+      }
+
+      state = result;
+    }
+
+    _refreshing ??= _internalRefresh();
+    return _refreshing!;
+  }
+
+  Future<String> add({
+    required String username,
+    required String password,
+    bool admin = false,
+    Map<String, dynamic>? data,
+    String? name,
+  }) async {
+    AmseApi api = ref.read(apiProvider);
+    final r = await api.users.create(
+        password: password,
+        username: username,
+        admin: admin,
+        data: data,
+        name: name);
+    await refresh();
+    return r;
+  }
+
+  Future<void> delete(User user) async {
+    AmseApi api = ref.read(apiProvider);
+
+    await api.users.delete(user.id);
+    await refresh();
+  }
+}
+
+final userProvider = StateNotifierProvider<UserNotifier, List<User>>(
+  (ref) => UserNotifier(ref),
+);
